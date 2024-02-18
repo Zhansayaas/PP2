5c10144d0a4a90f40dfc9f11a52406bf34a52445
Zhansayaas <karboz.zhansaya@bk.ru>
2024-02-18 17:32:16 +0600

    Initial Commit

---
+++ 1lab/Lab1W3school/datatypes.py
@@ -0,0 +1,27 @@
+#EX 1
+x = 5
+print(type(x))
+#type is int
+#EX 2
+x = "Hello World"
+print(type(x))
+#type is str
+#EX 3
+x = 20.5
+print(type(x))
+#Float
+#EX 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#list
+#EX 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#tuple
+#EX 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#dict
+#EX 7 
+x = True
+print(type(x))#bool
